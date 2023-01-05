639a744cd7c3f5f949cd987c7f0824248ae487dd
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-01-05 14:20:00 -0300

    Adiciona requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+const talkerJason = path.resolve(__dirname, './talker.json');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +12,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {  
+  const talkerDirect = JSON.parse(await fs.readFile(path.resolve(talkerJason)));
+ return res.status(200).json(talkerDirect);
 });
 
 app.listen(PORT, () => {
